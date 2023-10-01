9690ae68f58056480e4772d69dd832fd4351e767
taldoflemis <coderflemis@gmail.com>
2023-10-01 08:31:47 -0300

    change ast.ml

--- lib/ast.ml
+++ lib/ast.ml
@@ -6,7 +6,6 @@ type node =
 and expression =
   | Identifier of identifier
   | Integer of int
-  | Boolean of bool
   | String of string
   | Prefix of { operator : Token.t; right : expression }
   | Infix of { left : expression; operator : Token.t; right : expression }
@@ -15,17 +14,9 @@ and expression =
       consequence : block;
       alternative : block option;
     }
-  | FunctionLiteral of { parameters : identifier list; body : block }
-  | Call of { fn : expression; args : expression list }
-  | Array of expression list
-  | Index of { left : expression; index : expression }
-  | Hash of (expression * expression) list
-  | Macro of { parameters : identifier list; body : block }
 [@@deriving show { with_path = false }, sexp]
 
 and statement =
-  | Let of { name : identifier; value : expression }
-  | Return of expression
   | ExpressionStatement of expression
   | BlockStatement of block
 [@@deriving show { with_path = false }, sexp]
