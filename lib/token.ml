4a83e35537af3f0bc6a70625346efd33ffbceb01
taldoflemis <coderflemis@gmail.com>
2023-10-01 08:29:50 -0300

    initial commit

---
+++ lib/token.ml
@@ -0,0 +1,28 @@
+type t =
+  | Illegal
+  (* Items *)
+  | Ident of string
+  | Integer of string
+  | String of string
+  (* Operators *)
+  | Assign
+  | Plus
+  | Minus
+  | Asterisk
+  | Slash
+  | LessThan
+  | GreaterThan
+  | Equal
+  | NotEqual
+  (* Delimiters *)
+  | Semicolon
+  (* Keyword *)
+  | If
+  | Else
+  | While
+  | Read
+  | Write
+  | Prog
+  | Begin
+  | End
+[@@deriving show, eq, sexp]
